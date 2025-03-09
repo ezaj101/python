bc2ec7937fa47038252269ab0f1cdefb4278203b
ezaj101 <135958059+ezaj101@users.noreply.github.com>
2025-03-10 04:51:58 +0600

    Update 3-Range function.py

--- 3-Range function.py
+++ 3-Range function.py
@@ -1,4 +1,4 @@
-num = list(range(10)) # It starts with 0, so if you count from 0 to 9, it's a 10 number.
+num = list(range(10)) # It starts with 0, so if you count from 0 to 9, it's 10 number.
 print(num) # Output = 0, 1, 2, 3, 4, 5, 6, 7, 8, 9
 
 print(num[2]) # Output = 2. If you put 0, it will print 0; if you put 1, it will print 1.
