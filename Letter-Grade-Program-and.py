6830d26fa5ad4e7d59443378bf410c6796e2aec3
ezaj101 <135958059+ezaj101@users.noreply.github.com>
2025-07-21 15:37:39 +0600

    Update Letter-Grade-Program-and.py

--- Letter-Grade-Program-and.py
+++ Letter-Grade-Program-and.py
@@ -3,3 +3,11 @@ if marks >=80 and marks <=100: #or we can use (80 <= marks <= 100:)
     print("A+")
 elif marks >=70 and marks <=79: #or we can use (70 <= marks <= 79:)
     print("A")
+
+short way
+
+marks = 80
+if 80 <= marks <= 100:
+    print("A+")
+elif 70 <= marks <= 79:
+    print("A")
