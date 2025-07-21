70ecd9fd7b8c3133cb68800bda6fc5834bed420d
ezaj101 <135958059+ezaj101@users.noreply.github.com>
2025-07-21 15:57:14 +0600

    Update Letter-Grade-Program-and.py

--- Letter-Grade-Program-and.py
+++ Letter-Grade-Program-and.py
@@ -1,13 +1,17 @@
 marks = 80
-if marks >=80 and marks <=100: #or we can use (80 <= marks <= 100:) 
+if marks >=80 and marks <=100: 
     print("A+")
-elif marks >=70 and marks <=79: #or we can use (70 <= marks <= 79:)
+elif marks >=70 and marks <=79: #iF WE WANT MORE LINE TO ADD THEN TYPE (elif)
     print("A")
+elif marks <= 70:
+    print("F")
 
-short way
+#Or short way with different code
 
 marks = 80
 if 80 <= marks <= 100:
     print("A+")
 elif 70 <= marks <= 79:
     print("A")
+else:
+    print("F")
